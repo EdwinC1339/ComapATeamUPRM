0733b2d167397a14b303d9bbb266c416d7a17d9d
EdwinC1339 <edwin.camuy@upr.edu>
2023-02-16 19:27:53 -0400

    Set up reading CSV with pandas

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ print("Running on Pandas Version", pandas.__version__)
 
 
 def main():
-    pass
+    path = "Data/Problem_C_Data_Wordle.xlsx - Sheet1.csv"
+    df = pandas.read_csv(path).dropna(axis=1)
+    attempt_data = df.drop(['Date', 'Contest number', 'Number of  reported results', 'Number in hard mode'], axis=1)
+
+    print(attempt_data)
 
 
 if __name__ == "__main__":
