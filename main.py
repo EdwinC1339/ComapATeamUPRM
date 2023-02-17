b6eec50d7155faafbbdc706cfcb29f75eed02e9e
Lo-pez <diego.lopez13@upr.edu>
2023-02-16 21:23:51 -0400

    Mean squared error

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ def main():
 
     keys = ['Date', 'Contest number', 'Number of  reported results', 'Number in hard mode']
     attempt_data = wordle_df.drop(keys, axis=1).set_index('Word')
+    attempt_data2 = attempt_data.iloc[:150,:]
+    attempt_data3 = attempt_data.iloc[150:,:]
 
     # Add column with frequency information
     def freq(word: str):
@@ -33,11 +35,25 @@ def main():
             w += count
         return s/w
 
-    attempt_data['Mean # of Tries'] = attempt_data.apply(lambda x: mean_tries([
+    attempt_data2['Mean # of Tries'] = attempt_data2.apply(lambda x: mean_tries([
+        x['1 try'], x['2 tries'], x['3 tries'], x['4 tries'], x['5 tries'], x['6 tries'], x['7 or more tries (X)']]),
+                                                         axis=1)
+    attempt_data3['Mean # of Tries'] = attempt_data3.apply(lambda x: mean_tries([
         x['1 try'], x['2 tries'], x['3 tries'], x['4 tries'], x['5 tries'], x['6 tries'], x['7 or more tries (X)']]),
                                                          axis=1)
 
     print(attempt_data.head())
+    
+    from sklearn.metrics import mean_squared_error
+    
+    tries_mean = attempt_data2['Mean # of Tries'].mean()
+    attempt_data3['mean'] = tries_mean
+    print(attempt_data3['mean'])
+    
+    mse = mean_squared_error(attempt_data3['Mean # of Tries'], attempt_data3['mean'])
+    print(mse)
+    
+
 
 
 if __name__ == "__main__":
