f05541621864f03aae3ee9d8050736faea83d1eb
EdwinC1339 <edwin.camuy@upr.edu>
2023-02-16 20:05:20 -0400

    Added word frequency data

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ print("Running on Pandas Version", pandas.__version__)
 
 
 def main():
-    path = "Data/Problem_C_Data_Wordle.xlsx - Sheet1.csv"
-    df = pandas.read_csv(path).dropna(axis=1)
-    attempt_data = df.drop(['Date', 'Contest number', 'Number of  reported results', 'Number in hard mode'], axis=1)
+    wordle_path = "Data/Problem_C_Data_Wordle.xlsx - Sheet1.csv"
+    word_path = "Data/unigram_freq.csv"
 
-    print(attempt_data)
+    wordle_df = pandas.read_csv(wordle_path).dropna(axis=1)
+    word_df = pandas.read_csv(word_path).set_index('word')
+
+    keys = ['Date', 'Contest number', 'Number of  reported results', 'Number in hard mode']
+    attempt_data = wordle_df.drop(keys, axis=1).set_index('Word')
+
+    print(attempt_data.head())
+    print(word_df.head())
+
 
 
 if __name__ == "__main__":
